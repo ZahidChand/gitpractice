2ee407a7ca6c84e54f40b981309c2c10204ff70f
zahid <zahid@webassic.com>
2021-11-19 14:26:54 +0530

    Multiply Added

---
+++ Multiply.java
@@ -0,0 +1,20 @@
+
+package hello;
+
+/**
+ *
+ * @author adminrb
+ */
+public class Welcome {
+
+    
+    public static void main(String[] args) 
+    {
+       int a ,b,c;
+	a=10;b=5;
+	c = a*b;
+	system.out.printn(c);
+      
+    }
+    
+}
