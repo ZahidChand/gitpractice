571046906a16461eb55e1437e55753065d4414e5
zahid <zahid@webassic.com>
2021-11-19 14:36:21 +0530

    Sum Of 2 Nos

---
+++ Sum.java
@@ -0,0 +1,20 @@
+
+package hello;
+
+/**
+ *
+ * @author adminrb
+ */
+public class Welcome {
+
+    
+    public static void main(String[] args) 
+    {
+       int a ,b,c;
+	a=5;b=15;
+	c = a+b;
+	system.out.printn(" Sum Of A and B = "+c);
+      
+    }
+    
+}
