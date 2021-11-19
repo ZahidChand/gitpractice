748c4906f6b85298ec1143810ff1eef8a3c92234
zahid <zahid@webassic.com>
2021-11-19 13:03:21 +0530

    Welcome Commit

---
+++ Welcome.java
@@ -0,0 +1,17 @@
+
+package hello;
+
+/**
+ *
+ * @author adminrb
+ */
+public class Welcome {
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Welcome");
+      
+    }
+    
+}
